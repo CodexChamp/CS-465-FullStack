c2ed999c051375f9c29c6da0aa10892f3a1d161b
Rylan Champion <rylanchamp12@gmail.com>
2025-03-20 14:49:42 -0400

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+ var trips = JSON.parse(fs.readFileSync('.data/trips.json','utf8')); 
 /*Gets the homepage*/
 const travel = (req, res) => {
     res.render('travel', { title: 'Travlr Getaways' });
