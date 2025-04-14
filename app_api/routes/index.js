7d64291a2245bdfc6d9992def52584f8c52398a5
Rylan Champion <rylanchamp12@gmail.com>
2025-04-13 20:11:55 -0400

    Module 6 Completed Baseline origin

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,10 +1,20 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express'); // Express app
+const router = express.Router(); // Router Logic
 
-const tripsController = require("../controllers/trips");
+//this is where we import the controllers we will route
+const tripsController = require('../controllers/trips');
 
-router.route("/trips").get(tripsController.tripsList);
+//define route for our trips endpoint
+router
+    .route('/trips')
+    .get(tripsController.tripsList) // GET Method routes tripList
+    .post(tripsController.tripsAddTrip);
 
-router.route("/trips/:tripCode").get(tripsController.tripsFindByCode);
+// GET Method routes tripsFindByCode - requires parameter
+// PUT Methond routes tripsUpdateTrip - requires parameter
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
